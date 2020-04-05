9a13b8ad2bb6d1d257d1684be6bb1b873290c6f7
Callum Wilson <calwilson@live.co.uk>
2020-04-05 12:04:23 +0100

    Fixed compiler errors and formatted example file

--- c-cpp/example.cpp
+++ c-cpp/example.cpp
@@ -1,21 +1,15 @@
-#include <stdio>
+#include <iostream>
 
 namespace MyNamespace {
 // No indent in namespace body
 
 template<class T>
 class MyContainer {
-// Note: no indentation for public/private
+    // Note: no indentation for public/private
 public:
-    MyContainer(T& val)
-        : m_mem(val)
-    {
-    };
+    MyContainer(T val) : m_mem(val) {}
 
-    T get_val() const
-    {
-        return m_mem;
-    }
+    T get_val() const { return m_mem; }
 
 private:
     T m_mem;
@@ -23,20 +17,21 @@ private:
 } // namespace MyNamespace
 
 
-int main(int argv, char** argv)
-{   // Braces for functions go on new line
-    if (argv > 1) { // Space between control keyword (if, else if, for, do, while) and ()
+int main(int argc, char** argv)
+{ // Braces for functions go on new line
+    if (argc > 1) { // Space between control keyword (if, else if, for, do, while) and ()
                     // and space between ) {
         std::cout << "Arguments:" << std::endl;
-        for (int i = 0; i < argv; i++) {
+        for (int i = 0; i < argc; i++) {
             std::cout << "Arg " << i << ": " << argv[i] << std::endl;
         }
     }
-    else {  // else goes on new line
+    else { // else goes on new line
         std::cout << "No arguments passed" << std::endl;
     }
 
-    MyContainer<int> my_int_container{354};
+    int my_int = 354;
+    MyNamespace::MyContainer<int> my_int_container(my_int);
 
     std::cout << "MyContainer holds: " << my_int_container.get_val() << std::endl;
 
